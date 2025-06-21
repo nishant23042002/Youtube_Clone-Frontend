47e0da9f1d4f2fb53a9217a824b7b41b6e05e800
nishant23042002 <149838291+nishant23042002@users.noreply.github.com>
2025-06-22 01:36:57 +0530

    youtube-clone frontend folder structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+      <h1 className='font-bold text-red-600'>Helo</h1>  
+    </>
+  )
+}
+
+export default App
